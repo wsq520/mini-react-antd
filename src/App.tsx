e37801272989f4d8e084257404c1c705ed80cd6b
wsq <1224703359@qq.com>
2023-12-11 23:34:44 +0800

    修改button测试文件和实现Menu组件部分功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import Button, { ButtonType, ButtionSize } from "./components/Button/button";
+import Button from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
     <div className="App">
-      <Button>按钮</Button>
-      <Button btnType={ButtonType.Primary} size={ButtionSize.Large}>
-        按钮
-      </Button>
-      <Button btnType={ButtonType.Link} size={ButtionSize.Small} href="http://www.baidu.com">
-        链接按钮
-      </Button>
+      <div>
+        {" "}
+        <Button>按钮</Button>
+        <Button btnType='primary' size='lg'>
+          按钮
+        </Button>
+        <Button
+          btnType='link'
+          size='sm'
+          href="http://www.baidu.com"
+        >
+          链接按钮
+        </Button>
+      </div>
+      <Menu defaultIndex={0} onSelect={(index) => console.log(index)}>
+        <MenuItem index={0}>link</MenuItem>
+        <MenuItem index={1}>link</MenuItem>
+        <MenuItem index={2}>link</MenuItem>
+      </Menu>
     </div>
   );
 }
--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import Button, { ButtonProps, ButtonType, ButtionSize } from "./button";
+import Button, { ButtonProps } from "./button";
 
 const testProps: ButtonProps = {
-  btnType: ButtonType.Primary,
-  size: ButtionSize.Small,
+  btnType: "primary",
+  size: "sm",
   className: "klass",
 };
 
@@ -25,32 +25,36 @@ describe("button component test", () => {
     render(<Button>button</Button>);
     const element = screen.queryByText("button");
     expect(element).toBeInTheDocument();
-    if(!element) throw new Error("element is null");
+    if (!element) throw new Error("element is null");
     expect(element.tagName).toEqual("BUTTON");
     expect(element).toHaveClass("btn btn-default");
   });
-  
+
   it("should render button based on different props", () => {
     render(<Button {...testProps}>Nice</Button>);
-    const element = screen.getByText('Nice');
+    const element = screen.getByText("Nice");
     expect(element).toBeInTheDocument();
-    expect(element).toHaveClass('btn-primary btn-sm klass');
+    expect(element).toHaveClass("btn-primary btn-sm klass");
   });
 
   it("should render link button", () => {
-    render(<Button btnType="link" href="http://www.baidu.com">Link</Button>)
-    const element = screen.getByText('Link')
-    expect(element).toBeInTheDocument()
-    expect(element.tagName).toEqual('A')
-    expect(element).toHaveClass('btn btn-link')
+    render(
+      <Button btnType="link" href="http://www.baidu.com">
+        Link
+      </Button>
+    );
+    const element = screen.getByText("Link");
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("A");
+    expect(element).toHaveClass("btn btn-link");
   });
 
-  it('should render disabled button when disabled set to true', () => {
-    render(<Button {...disabledProps}>Nice</Button>)
-    const element = screen.getByText('Nice') as HTMLButtonElement
-    expect(element).toBeInTheDocument()
-    expect(element.disabled).toBeTruthy()
-    fireEvent.click(element)
-    expect(disabledProps.onClick).not.toHaveBeenCalled()
-  })
+  it("should render disabled button when disabled set to true", () => {
+    render(<Button {...disabledProps}>Nice</Button>);
+    const element = screen.getByText("Nice") as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.disabled).toBeTruthy();
+    fireEvent.click(element);
+    expect(disabledProps.onClick).not.toHaveBeenCalled();
+  });
 });
